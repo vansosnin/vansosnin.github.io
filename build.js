216899917a9d1ff85aa7f941190024ad4e765391
Ivan Sosnin <vansosnin@gmail.com>
2018-12-05 23:45:40 +0500

    Add RSS

--- build.js
+++ build.js
@@ -5,12 +5,19 @@ const collections = require('metalsmith-collections');
 const discoverPartials = require('metalsmith-discover-partials');
 const datePlugin = require('metalsmith-date-in-filename');
 const permalinks = require('metalsmith-permalinks');
+const feed = require('metalsmith-feed');
 const formatDate = require('./plugins/formatDate');
 
 Metalsmith(__dirname)
     .metadata({
         sitename: 'Ivan Sosnin',
         role: 'Frontend developer',
+        // for RSS feed
+        site: {
+            title: 'Ivan Sosnin blog',
+            url: 'https://vansosnin.github.io/',
+            author: 'Ivan Sosnin',
+        },
     })
     .source('./src')
     .destination('./public')
@@ -23,6 +30,7 @@ Metalsmith(__dirname)
             },
         })
     )
+    .use(feed({ collection: 'posts', limit: false, destination: 'rss.xml' }))
     .use(datePlugin({ override: true }))
     .use(formatDate())
     .use(markdown())
